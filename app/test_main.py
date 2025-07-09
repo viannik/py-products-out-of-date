1ac13a7898a7c8f8f0f7c6a157b3e5eb6fe88275
viannik <ivan.nikitski@gmail.com>
2025-07-10 01:16:28 +0300

    add unit tests for outdated_products function

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,60 @@
-# write your code here
+import datetime
+from unittest.mock import patch, MagicMock
+from pytest import mark
+
+from app.main import outdated_products
+
+
+@mark.parametrize(
+    "products, today_date, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600,
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160,
+                },
+            ],
+            datetime.date(2022, 2, 5),
+            ["duck"],
+        ),
+        (
+            [
+                {
+                    "name": "today_product",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120,
+                },
+            ],
+            datetime.date(2022, 2, 5),
+            [],
+        ),
+        (
+            [
+                {
+                    "name": "yesterday_product",
+                    "expiration_date": datetime.date(2022, 2, 4),
+                    "price": 100,
+                },
+            ],
+            datetime.date(2022, 2, 5),
+            ["yesterday_product"],
+        ),
+    ],
+)
+@patch("app.main.datetime.date")
+def test_outdated_products(
+    mock_date: MagicMock,
+    products: list,
+    today_date: datetime.date,
+    expected: list,
+) -> None:
+    mock_date.today.return_value = today_date
+
+    assert outdated_products(products) == expected
